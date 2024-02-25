a00ac321d238e4106ed31a6e8f5b746fe73c02d7
abdubest101010 <101792283+abdubest101010@users.noreply.github.com>
2024-02-26 00:44:28 +0300

    Add files via upload

---
+++ Form/Java/Script.js
@@ -0,0 +1,13 @@
+// $("#submit").on("click", function userName(e){
+//     e.preventDefault();
+// const username1=document.getElementById("users");
+// var userNameRex=^[a-zA-Z0-9_-]{3,16}$;
+// if(username1.value===''){
+//     username1.placeholder="please enter username";
+
+// $("#users").css("backgroundColor", "red")
+// }
+//     // else if(!userName.value==userName){
+//     //     userName.innerHTML="PLEASE TRY AGAIN"
+//     // }
+// })
