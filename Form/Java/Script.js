c20a9f02990f5fb3f07f6b35d2875422869c59ea
abdubest101010 <101792283+abdubest101010@users.noreply.github.com>
2024-02-26 23:20:58 +0300

    css part slightly

--- Form/Java/Script.js
+++ Form/Java/Script.js
@@ -1,13 +1,13 @@
-// $("#submit").on("click", function userName(e){
-//     e.preventDefault();
-// const username1=document.getElementById("users");
-// var userNameRex=^[a-zA-Z0-9_-]{3,16}$;
-// if(username1.value===''){
-//     username1.placeholder="please enter username";
+$("#submit").on("click", function userName(e) {
+  e.preventDefault();
+  const username1 = document.getElementById("users");
+  const errorMessage = document.querySelector(".error");
 
-// $("#users").css("backgroundColor", "red")
-// }
-//     // else if(!userName.value==userName){
-//     //     userName.innerHTML="PLEASE TRY AGAIN"
-//     // }
-// })
+  var userNameRex = /^[a-zA-Z0-9_-]{3,16}$/;
+  if (username1.value === "") {
+    errorMessage.innerHTML = "please enter username";
+  } else if (!userNameRex.test(username1.value)) {
+    console.log(username1.value);
+    errorMessage.innerHTML = "please enter correctly";
+  }
+});
